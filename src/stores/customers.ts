a789f617f8b9b249cd0a391f4079abf906528a47
John Faria <johnwbf@hotmail.com>
2023-01-17 08:28:21 -0300

    feat: add crud methods on customer store

--- src/stores/customers.ts
+++ src/stores/customers.ts
@@ -2,17 +2,10 @@ import { Ref, ref } from 'vue';
 import { defineStore } from 'pinia';
 import { Customer, CustomerSchema } from '../schemas/customers';
 import { v4 as uuidv4 } from 'uuid';
+import product from './data.json';
 
 export const useCounterStore = defineStore('counter', () => {
-  const customers: Ref<Customer[]> = ref([
-    {
-      id: '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d',
-      name: 'Jon Doe',
-      email: 'jondoe@email.com',
-      active: true,
-      products: [],
-    },
-  ]);
+  const customers: Ref<Customer[]> = ref(product);
 
   const validateCustomer = (customer: Customer) => {
     const result = CustomerSchema.safeParse(customer);
@@ -23,5 +16,30 @@ export const useCounterStore = defineStore('counter', () => {
     customers.value.push({ id: uuidv4(), ...customer });
   };
 
-  return { customers, createCustomer, validateCustomer };
+  const getCustomerById = (id: string) => {
+    return customers.value.find((el) => el.id === id);
+  };
+
+  const updateCustomerById = (customer: Customer) => {
+    let result = customers.value.find((el) => el.id === customer.id);
+    if (result) {
+      result = customer;
+    }
+  };
+
+  const deleteCustomerById = (customer: Customer) => {
+    const objIndex = customers.value.findIndex((obj) => obj.id === customer.id);
+    if (objIndex > -1) {
+      customers.value.splice(objIndex, 1);
+    }
+  };
+
+  return {
+    customers,
+    createCustomer,
+    validateCustomer,
+    getCustomerById,
+    updateCustomerById,
+    deleteCustomerById,
+  };
 });
