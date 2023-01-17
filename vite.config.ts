c14c65ed8340ddf15b0cda6b38fa095f9147b6bd
John Faria <johnwbf@hotmail.com>
2023-01-16 22:30:26 -0300

    deps: setup eslint, stylelint and vite plugin checker

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [
+    vue(),
+    checker({
+      typescript: true,
+      vueTsc: true,
+      eslint: {
+        lintCommand: 'eslint "./src/**/*.{ts,tsx,vue}"', // for example, lint .ts & .tsx
+      },
+      stylelint: {
+        // for example, lint .css and .vue
+        lintCommand: 'stylelint ./src/**/*.{scss,vue}',
+      },
+    }),
+  ],
+});
